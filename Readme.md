5ccbfdcd2d32ae973a5b3528927ceb79161759f8
Rodrigo da Cunha Pereira <RodrigoNegao@users.noreply.github.com>
2020-06-28 22:35:51 -0300

    add filters

--- Readme.md
+++ Readme.md
@@ -1,3 +1,6 @@
 # App de Curso feito em RN
 
 - Neste projeto foi as dependências conforme o file "package.json"
+
+## Futuramente:
+[Expo Profile](https://expo.io/@rodrigonegao)
--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,13 +1,49 @@
-import React from 'react';
-import {View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {View, Text, StyleSheet, Switch , Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import HeaderButton from '../components/HeaderButton';
+import Colors from '../constants/Colors';
+
+const FilterSwith = props =>{
+        return(
+            <View style={styles.filterContainer}>
+                <Text>{props.label}</Text>
+                <Switch 
+                        trackColor={{true: Colors.primaryColor}}
+                        thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
+                        value={props.state} 
+                        onValueChange={props.onChange}
+                 />
+            </View>
+    );
+};
 
 const FiltersScreen = props => {
+    const [isGlutenFree, setIsGlutenFree] = useState (false);
+    const [isLactoseFree, setIsLactoseFree] = useState (false);
+    const [isVeganFree, setIsVeganFree] = useState (false);
+    const [isVegetarianFree, setIsVegetarianFree] = useState (false);
+
     return (
         <View style={styles.screen}>
-            <Text>The Filters Screen</Text>
+            <Text style={styles.title}> Tipos de Filtro/ Restrições </Text>
+            <FilterSwith label='Gluten-free' 
+                         state={isGlutenFree}
+                         onChange={newValue => setIsGlutenFree(newValue)}
+            />
+            <FilterSwith label='Lactose-free' 
+                         state={isLactoseFree}
+                         onChange={newValue => setIsLactoseFree(newValue)}
+            />
+            <FilterSwith label='Vegan' 
+                         state={isVeganFree}
+                         onChange={newValue => setIsVeganFree(newValue)}
+            />
+            <FilterSwith label='Vegetarian' 
+                         state={isVegetarianFree}
+                         onChange={newValue => setIsVegetarianFree(newValue)}
+            />
         </View>
     );
 };
@@ -29,9 +65,29 @@ FiltersScreen.navigationOptions = navData => {
 const styles = StyleSheet.create({
     screen:{
         flex:1,
-        justifyContent: 'center',
+        //justifyContent: 'center',
         alignItems : 'center',
     },
+    title:{
+        fontFamily: 'open-sans-bold',
+        fontSize: 22,
+        margin: 20,
+        textAlign: 'center'
+    },
+    filterContainer:{
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '80%',
+        marginVertical: 10
+    }
 });
 
 export default FiltersScreen;
+
+                // <Switch 
+                //         trackColor={{true: Colors.primaryColor}}
+                //         thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
+                //         value={isGlutenFree} 
+                //         onValueChange={newValue => setIsGlutenFree(newValue)}
+                //  />
